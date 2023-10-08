bb9cc8168b19a22c8c1137232a89ede99996f6d9
Edward Stanford <edwardstanford7@gmail.com>
2023-10-08 12:14:33 -0600

    a bit more finetuning

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,24 +3,18 @@ extends CharacterBody2D
 @export var collision: CollisionShape2D
 
 @export var friction: float = 0.075
-@export var speed_force: float = 2000 # ikik DC can suck a fat one
-@export var jump_force: float = -30000
-@export var pull_push_force: float = 150000
+@export var speed_force: float = 200000 # ikik DC can suck a fat one
+@export var jump_force: float = -2000000
+@export var pull_push_force: float = 15000000
 @export var mass: float = 65
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var selected_metal
 var force_per_frame: Vector2
-var moving_left = false
 
 func _physics_process(delta):
 	force_per_frame = Vector2(0, 0)
-	
-	if velocity.x < 0:
-		moving_left = true
-	else:
-		moving_left = false
 	
 	handle_gravity()
 	handle_jump_input()
@@ -28,7 +22,7 @@ func _physics_process(delta):
 	handle_iron_steel_input()
 	handle_friction()
 	
-	velocity += force_per_frame * delta
+	velocity += force_per_frame * delta / mass
 	
 	move_and_slide()
 
@@ -42,10 +36,10 @@ func handle_friction():
 		
 	if is_on_wall():
 		var collision = get_slide_collision(0)
-		if moving_left && force_per_frame.x < 0:
+		if force_per_frame.x < 0:
 			force_per_frame.y -= friction * abs(force_per_frame.x) * velocity.y / 10
 			return
-		if !moving_left && force_per_frame.x > 0:
+		if force_per_frame.x > 0:
 			force_per_frame.y -= friction * force_per_frame.x * velocity.y / 10
 			return
 
@@ -54,7 +48,7 @@ func handle_gravity():
 
 func handle_jump_input():
 	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
+		if is_on_floor() || is_on_wall():
 			force_per_frame.y += jump_force
 
 func handle_move_input():
